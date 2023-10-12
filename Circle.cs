1a3a663c4758bbbb4708f1ecb1f0367b27c73b0b
Bumbaclat89 <HANY89@LIVE.SE>
2023-10-12 18:08:46 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrundernaOOP
+{
+    public class Circle
+    {
+        float _pi = 3.141f; //här skapar vi fält både float och int
+        int _Radius = 0;
+
+      
+        public Circle (int radius) //skapar constructor som hjälper oss att dela ett värde till radien
+        {
+            _Radius = radius; 
+        }
+
+        public float GetArea() //skapar metod som ska anropas via main
+        {
+            float area = _Radius * _Radius * _pi; //skapar area som ska räknas arean 
+            Console.WriteLine("en Cirkel med radie " + _Radius + " har " + area + " area");
+                return area; //retunerar ett värde
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GrundernaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle size = new Circle(5); //Här skapar vi objektet som är kopplat till circle klassen
+            size.GetArea(); //Här anropar vi metoden 
+            Circle size2 = new Circle(6);
+            size2.GetArea();
+
+        }
+    }
+}
